67849d824d73075912014b57da3cb0ffa374f93e
yurimendes001 <yuricardosomendes41@gmail.com>
2023-02-28 08:22:09 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# html-css
+ curso HTML5 CSS3
---
+++ exercicios/ex018/index.js
@@ -0,0 +1,14 @@
+// Definir uma variável
+var x = 5;
+
+// Imprimir no console
+console.log("O valor de x é " + x);
+
+// Criar uma função
+function dobro(numero) {
+  return numero * 2;
+}
+
+// Usar a função
+var resultado = dobro(x);
+console.log("O dobro de x é " + resultado);
